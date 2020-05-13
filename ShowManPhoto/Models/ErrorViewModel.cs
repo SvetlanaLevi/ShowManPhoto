6d59328f034991471f5d0984ca1f4c36313c98bb
Levi <Laytika@icloud.com>
2020-05-14 02:11:03 +0300

    MVC Project created

---
+++ ShowManPhoto/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShowManPhoto.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
